f8eed5de1aabcdbdd82a0ceecbaabf8b5c5bb359
Sammy Knight <knight.samuel@outlook.com>
2019-06-13 08:24:02 +0100

    Added main and commands with functionality for list and show.

--- oxide/src/commands.rs
+++ oxide/src/commands.rs
@@ -1,18 +1,79 @@
-use std::collections::HashMap;
+use std::io;
+use std::path::{Path, PathBuf};
+use std::fs::{read_dir, read_to_string};
 
-// static commands: HashMap<&str, fn(&String)>  = vec!
-// [
-//     ("ls", List),
-//     ("list", List),
-//     ("dir", List),
-//     ("cat", Display),
-//     ("display", Display)
-// ].into_iter().collect(); 
+pub fn list(filepath: &Vec<String>, cwd: &mut PathBuf) {
+    if filepath.len() == 0 {
+        _list(cwd);
+        return;
+    }
+    
+    let filepaths: Vec<&Path> = filepath.iter().map(Path::new).collect();
+    if filepaths.len() == 1 {
+        _list(filepaths[0]);
+        return;
+    }
 
-fn List (filepath: &String) {
-    println!("In List!");
+    for path in filepaths {
+        println!("{}", path.display());
+        _list(path);
+    }
+
+
 }
 
-fn Display (filepath: &String) {
-    println!("In Display!");
+fn _list(filepath: &Path) -> io::Result<()> {
+    if filepath.is_dir() {
+        for entry in read_dir(filepath)? {
+            let entry = entry?;
+            match entry.file_name().to_str() {
+                None => {
+                    println!("Could not convert path to utf-8 string. What funky OS are you using?");
+                }
+                Some(filename) => {
+                    println!("{}", filename);
+                }
+            }
+        }
+    }
+    else {
+        println!("Can't list a file! But the file name is: {}", filepath.display());
+    }
+    Ok(())
+}
+
+
+pub fn change_directory(filepath: &Vec<String>, cwd: &mut PathBuf) {
+
+}
+
+fn _change_directory(filepath: &Path) {
+
+}
+
+
+pub fn show (filepath: &Vec<String>, cwd: &mut PathBuf) {
+    if filepath.len() == 0 {
+        println!("Pass in a file and I will SHOW you the contents");
+        return;
+    }
+    
+    let filepaths: Vec<&Path> = filepath.iter().map(Path::new).collect();
+    
+    for path in filepaths {
+        _show(path);
+    }    
+}
+
+fn _show(filepath: &Path) {
+    if !filepath.is_file() {
+        println!("Can not print out non-file contents.");
+        return;
+    }
+    println!("Showing file {}", filepath.display());
+    let file_data = read_to_string(filepath);
+    match file_data {
+        Err(error) => { println!("Could not read file {} \n Error {}", filepath.display(), error); }
+        Ok(file_text) => { println!("{}", file_text); }
+    }
 }
--- oxide/src/main.rs
+++ oxide/src/main.rs
@@ -1,12 +1,32 @@
+#[macro_use]
+extern crate lazy_static;
+
 use std::io::{self, Write};
 use std::iter::FromIterator;
+use std::path::{Path, PathBuf};
+use std::collections::HashMap;
 mod commands;
+use commands::{list, show};
 
 const PROMPT: &str = ">> ";
 const DEBUG: bool = true;
 
+
+lazy_static! {
+    static ref COMMANDS: HashMap<String, fn(&Vec<String>, &mut PathBuf) -> ()> = {
+        let mut command_hm = HashMap::new();
+        command_hm.insert("ls".to_string(), list as fn(&Vec<String>, &mut PathBuf) -> ());
+        command_hm.insert("cat".to_string(), show);
+        //command_hm.insert("cd".to_string(), change_directory);
+
+        command_hm
+    };
+}
+
 fn main() {
     println!("Welcome to Oxide! A shell written entirely in Rust.");
+
+    let mut cwd = PathBuf::from(".");
 
     loop {
         print!("{}", PROMPT);
@@ -18,7 +38,7 @@ fn main() {
                 if DEBUG {
                     println!("Read the following: {}", input);
                 }
-                execute_command(&mut input);
+                execute_command(&mut input, &mut cwd);
             }
             Err(error) => {
                 println!("Error when reading from input: {}", error);
@@ -27,14 +47,29 @@ fn main() {
     }
 }
 
-fn execute_command(input: &mut String) {
-    println!("Executing on following string: {}", input);
+
+fn parse_command(input: &mut String) -> Vec<String> {
     let command_vector = input.split_whitespace();
     let commands: Vec<String> = Vec::from_iter(command_vector.map(String::from));
-    process_command(&commands[0], &commands[1..].to_vec());
+    commands
 }
 
-fn process_command(command: &String, inputs: &Vec<String>) {
+fn execute_command(input: &mut String, cwd: &mut PathBuf) {
+    println!("Executing on following string: {}", input);
+    // Change this in future to return an iterator of (command, args)
+    let commands: Vec<String> = parse_command(input);
+    process_command(&commands[0], &commands[1..].to_vec(), cwd);
+}
+
+fn process_command(command: &String, inputs: &Vec<String>, cwd: &mut PathBuf) {
     println!("Command: {}", command);
-    println!("Inputs: {:?}", inputs);   
+    println!("Inputs: {:?}", inputs); 
+    match COMMANDS.get(command) {
+        None => {
+            println!("Command {} not understood", command);
+        }
+        Some(comm) => {
+            comm(&inputs, cwd);
+        }
+    } 
 }
