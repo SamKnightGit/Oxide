8db32b5e4d2aee20e79eca3a8e9b3375c19692f6
Colin Parrott <colinparrott2@gmail.com>
2019-06-14 04:09:48 +0100

    Enhanced ls command

--- oxide/src/commands.rs
+++ oxide/src/commands.rs
@@ -1,7 +1,13 @@
+use core::fmt::Debug;
+use std::fs::{DirEntry, Metadata, metadata, read_dir, read_to_string};
 use std::io;
-use std::path::{Path, PathBuf};
-use std::fs::{read_dir, read_to_string};
 use std::io::Write;
+use std::path::{Path, PathBuf};
+use std::time::SystemTime;
+
+use chrono::DateTime;
+use chrono::Local;
+
 use crate::windows_clear;
 
 pub fn list(filepath: &Vec<String>, cwd: &mut PathBuf) {
@@ -9,7 +15,7 @@ pub fn list(filepath: &Vec<String>, cwd: &mut PathBuf) {
         _list(cwd);
         return;
     }
-    
+
     let filepaths: Vec<&Path> = filepath.iter().map(Path::new).collect();
     if filepaths.len() == 1 {
         _list(filepaths[0]);
@@ -20,8 +26,6 @@ pub fn list(filepath: &Vec<String>, cwd: &mut PathBuf) {
         println!("{}", path.display());
         _list(path);
     }
-
-
 }
 
 
@@ -34,18 +38,59 @@ fn _list(filepath: &Path) -> io::Result<()> {
                     println!("Could not convert path to utf-8 string. What funky OS are you using?");
                 }
                 Some(filename) => {
-                        println!("{}", filename);
+                    println!("{}", _form_list_string(&entry, &filename, filepath));
                 }
             }
         }
-    }
-    else if filepath.is_file() {
+    } else if filepath.is_file() {
         println!("Can't list a file! But the file name is: {}", filepath.display());
-    }
-    else{
+    } else {
         println!("Not a valid directory: {}", filepath.display());
     }
     Ok(())
+}
+
+fn _form_list_string(file: &DirEntry, filename: &str, file_path: &Path) -> String {
+    let metadata_result = file.metadata();
+
+    if metadata_result.is_ok() {
+        let metadata = metadata_result.unwrap();
+        return format!("{0: <6}  {1}  {2: <9}  {3}", _get_file_type_string(file), _get_file_modified(&metadata), _get_file_size(&metadata), filename);
+    } else {
+        return format!("{}", filename);
+    }
+}
+
+fn _get_file_size(metadata: &Metadata) -> String {
+    // TODO: fix directories being zero
+    // TODO: decide on system for displaying sizes in what units
+    let mut size: f64 = metadata.len() as f64;
+    size /= 1e3;
+
+    return format!("{:.2} KB", size);
+}
+
+fn _get_file_modified(metadata: &Metadata) -> String {
+    if metadata.modified().is_ok() {
+        let time = DateTime::<Local>::from(metadata.modified().unwrap());
+        return format!("{}", time.format("%d/%m/%Y %H:%M"));
+    }
+
+    return format!("");
+}
+
+fn _get_file_type_string(file: &DirEntry) -> &str {
+    let file_type = file.file_type().unwrap();
+    let mut string = "";
+
+    if file_type.is_file() {
+        string = "<file>";
+    } else if file_type.is_dir() {
+        string = "<dir>";
+    } else if file_type.is_symlink() {
+        string = "<sym>";
+    }
+    return string;
 }
 
 
@@ -66,33 +111,29 @@ pub fn change_directory(filepath: &Vec<String>, cwd: &mut PathBuf) {
 }
 
 fn _change_directory(filepath: &Path) {
-
     let path_string = filepath.to_str().unwrap();
 
-    if filepath.is_dir(){
+    if filepath.is_dir() {
         std::env::set_current_dir(filepath);
-    }
-    else if filepath.is_file(){
+    } else if filepath.is_file() {
         println!("\"{}\" is a file not a directory", path_string);
-    }
-    else {
+    } else {
         println!("\"{}\" no such file or directory", path_string);
     }
-
 }
 
 
-pub fn show (filepath: &Vec<String>, cwd: &mut PathBuf) {
+pub fn show(filepath: &Vec<String>, cwd: &mut PathBuf) {
     if filepath.len() == 0 {
         println!("Pass in a file and I will SHOW you the contents");
         return;
     }
-    
+
     let filepaths: Vec<&Path> = filepath.iter().map(Path::new).collect();
-    
+
     for path in filepaths {
         _show(path);
-    }    
+    }
 }
 
 fn _show(filepath: &Path) {
@@ -108,21 +149,21 @@ fn _show(filepath: &Path) {
     }
 }
 
-pub fn clear(filepath: &Vec<String>, cwd: &mut PathBuf){
+pub fn clear(filepath: &Vec<String>, cwd: &mut PathBuf) {
     _clear();
 }
 
 #[cfg(target_family = "unix")]
-fn _clear(){
+fn _clear() {
     std::process::Command::new("clear").status().unwrap();
 }
 
 #[cfg(target_family = "windows")]
-fn _clear(){
+fn _clear() {
     windows_clear::clear_screen_windows();
 }
 
-pub fn mkdir(filepath: &Vec<String>, cwd: &mut PathBuf){
+pub fn mkdir(filepath: &Vec<String>, cwd: &mut PathBuf) {
     let file_paths: Vec<&Path> = filepath.iter().map(Path::new).collect();
 
     for path in file_paths {
@@ -131,18 +172,18 @@ pub fn mkdir(filepath: &Vec<String>, cwd: &mut PathBuf){
 }
 
 fn _mkdir(path: &Path) {
-    if path.exists(){
+    if path.exists() {
         println!("{} already exists", path.display());
         return;
     }
     let result = std::fs::create_dir_all(path);
 
-    if result.is_err(){
+    if result.is_err() {
         println!("Failed to create directory: {}", path.display());
         println!("{}", result.err().unwrap());
     }
 }
 
-pub fn exit(filepath: &Vec<String>, cwd: &mut PathBuf){
+pub fn exit(filepath: &Vec<String>, cwd: &mut PathBuf) {
     std::process::exit(0)
 }
