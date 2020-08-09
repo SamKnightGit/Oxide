e0724f63f1dc51e1bac0384444784ebe37c8851a
SamKnightGit <knight.samuel@outlook.com>
2020-08-09 12:19:56 +0200

    Finished parser and building of ast.

--- oxide/src/parser.rs
+++ oxide/src/parser.rs
@@ -1,4 +1,18 @@
 use std::collections::HashSet;
+use std::fmt;
+
+type Result<T> = std::result::Result<T, ParseError>;
+
+#[derive(Debug, Clone)]
+struct ParseError {
+    message: String
+}
+
+impl fmt::Display for ParseError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "Parse Error: {}", self.message)
+    }
+}
 
 lazy_static! {
     static ref COMMANDS: HashSet<&'static str> =
@@ -10,116 +24,163 @@ lazy_static! {
     
     static ref REDIRECTION_OPS: HashSet<&'static str> = 
         [
-            ">", ">>", "<"
+            ">", ">>", "<", "|"
         ].iter().cloned().collect();
 }
+
+#[derive(Debug)]
+pub enum ParseNodeType {
+    Expr,
+    CommandExpr,
+    RedirectionExpr,
+    Command(String),
+    File(String),
+    RedirectionOp(String),
+    Pipe,
+}
+
+#[derive(Debug)]
+pub struct ParseNode {
+    entry: ParseNodeType,
+    children: Option<Vec<ParseNode>>,
+} 
+
 
 
 pub fn parse_input(input_tokens: &Vec<&str>)
 {
-    let mut input_index: usize = 0;
-    parse_expr(input_tokens, &mut input_index);
-
+    let mut input_index: usize = 0; 
+    match parse_expr(input_tokens, &mut input_index) {
+        Ok(ast) => println!("{:?}", ast),
+        Err(e) => println!("Parsing Error: {0}", e),
+    }
+    
 }
 
-fn parse_expr(input_tokens: &Vec<&str>, input_index: &mut usize) -> bool
+fn parse_expr(input_tokens: &Vec<&str>, input_index: &mut usize) -> Result<ParseNode>
 {
-    // Add Expr to AST node 
-    if !parse_command_expr(input_tokens, input_index)
-    {
-        return false
-    }
-    if !parse_redirection_expr(input_tokens, input_index)
-    {
-        return false
-    }  
-    return true
+    let mut parse_tree = ParseNode {
+        entry: ParseNodeType::Expr,
+        children: Some(Vec::new()),
+    };
+    
+    parse_command_expr(input_tokens, input_index, &mut parse_tree)?;
+   
+    parse_redirection_expr(input_tokens, input_index, &mut parse_tree)?;
+    
+    return Ok(parse_tree)
 }
 
-fn parse_command_expr(input_tokens: &Vec<&str>, input_index: &mut usize) -> bool
+fn parse_command_expr(input_tokens: &Vec<&str>, input_index: &mut usize, tree_node: &mut ParseNode) -> Result<()>
 {
-    if !parse_command(input_tokens, input_index)
-    {
-        return false
-    }
-    if !parse_filelist(input_tokens, input_index)
-    {
-        return false
-    }
-    return true
+    let mut command_expr_node = ParseNode {
+        entry: ParseNodeType::CommandExpr,
+        children: Some(Vec::new()),
+    };
+     
+    parse_command(input_tokens, input_index, &mut command_expr_node)?;
+    
+    parse_filelist(input_tokens, input_index, &mut command_expr_node)?;
+        
+    tree_node.children.as_mut().unwrap().push(command_expr_node);
+    Ok(())
 }
 
-fn parse_redirection_expr(input_tokens: &Vec<&str>, input_index: &mut usize) -> bool
+fn parse_redirection_expr(input_tokens: &Vec<&str>, input_index: &mut usize, tree_node: &mut ParseNode) -> Result<()>
 {
     if *input_index == input_tokens.len()
     {
-        return true
+        return Ok(())
     }
+
+    let mut redirection_expr_node = ParseNode {
+        entry: ParseNodeType::RedirectionExpr,
+        children: Some(Vec::new()),
+    };
 
     if input_tokens[*input_index] == "|"
     {
         // Add pipe to AST
-        if !parse_command_expr(input_tokens, input_index)
-        {
-            return false
-        }
+        let pipe_node = ParseNode {
+            entry: ParseNodeType::Pipe,
+            children: None,
+        };
+        redirection_expr_node.children.as_mut().unwrap().push(pipe_node);
+        *input_index += 1;
+        parse_command_expr(input_tokens, input_index, &mut redirection_expr_node)?;
+            
     }
     else
     {
-        if !parse_redirection_op(input_tokens, input_index)
-        {
-            return false
-        }
-        if !parse_filelist(input_tokens, input_index)
-        {
-            return false
-        }
+        parse_redirection_op(input_tokens, input_index, &mut redirection_expr_node)?;
+        
+        parse_filelist(input_tokens, input_index, &mut redirection_expr_node)?; 
     }
-    return true
+
+    tree_node.children.as_mut().unwrap().push(redirection_expr_node);
+    return Ok(())
 }
 
-fn parse_command(input_tokens: &Vec<&str>, input_index: &mut usize) -> bool
+fn parse_command(input_tokens: &Vec<&str>, input_index: &mut usize, tree_node: &mut ParseNode) -> Result<()>
 {
     let command = input_tokens[*input_index];
     if !COMMANDS.contains(command) {
-        println!("Failed to parse command: {0}", command);
-        return false  
+        return Err(ParseError { 
+            message: format!("could not understand command '{0}'", command) 
+        })
     }
 
     // Add Expr to AST node 
+    let command_node = ParseNode {
+        entry: ParseNodeType::Command(command.to_string()),
+        children: None,
+    };
+    tree_node.children.as_mut().unwrap().push(command_node);
     *input_index += 1;
-    return true
+    return Ok(())
 }
 
-fn parse_filelist(input_tokens: &Vec<&str>, input_index: &mut usize) -> bool
+fn parse_filelist(input_tokens: &Vec<&str>, input_index: &mut usize, tree_node: &mut ParseNode) -> Result<()>
 {
     // epsilon rule if all tokens have been parsed or not filename
     if *input_index == input_tokens.len()
     {
-        return true
+        return Ok(())
     }
     
     let token = input_tokens[*input_index];
     if is_filename(token)
     {
         // Add token as file to syntax tree
+        let file_node = ParseNode {
+            entry: ParseNodeType::File(token.to_string()),
+            children: None,
+        };
+        tree_node.children.as_mut().unwrap().push(file_node);
         *input_index += 1;
-        parse_filelist(input_tokens, input_index);
+        parse_filelist(input_tokens, input_index, tree_node)?;
     }
-    return true
+    return Ok(())
 }
 
-fn parse_redirection_op(input_tokens: &Vec<&str>, input_index: &mut usize) -> bool
+fn parse_redirection_op(input_tokens: &Vec<&str>, input_index: &mut usize, tree_node: &mut ParseNode) -> Result<()>
 {
     let token = input_tokens[*input_index];
     if !REDIRECTION_OPS.contains(token)
     {
-        println!("Expected redirection operator, got: {0}", token);
-        return false
+        return Err(ParseError {
+            message: format!("expected redirection operator, got '{0}'", token)
+        })
     }
     // Add token as redirection op to syntax tree
+    let redirection_op_node = ParseNode {
+        entry: ParseNodeType::RedirectionOp(token.to_string()),
+        children: None,
+    };
+
+    tree_node.children.as_mut().unwrap().push(redirection_op_node);
     *input_index += 1;
-    return true
+    return Ok(())
 }
 
 fn is_filename(word: &str) -> bool
